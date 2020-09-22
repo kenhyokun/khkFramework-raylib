7fa8f950c49438bac3987ae4f99b15f178120e1c
kevin <kevinhyokun91@gmail.com>
2020-09-22 14:53:40 +0700

    simple update
    basic implementation libgdx texture atlas.

--- khkFramework/TextureAtlas.h
+++ khkFramework/TextureAtlas.h
@@ -32,6 +32,7 @@
 #include<fstream>
 #include<string>
 #include<cctype>
+#include<vector>
 #include<map>
 
 using namespace std;
@@ -49,52 +50,59 @@ struct TextureAtlas{
   string texture_file_name;
 
   inline TextureAtlas(string file_src){
+
+    // load atlas file
     ifstream file(file_src);
     string line;
     if(file.is_open()){
       while(getline(file, line)){
-
 	line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end()); // trim line
-
-	if(!_IsHas(line, ":") &&
-	   !_IsHas(line, ",") &&
-	   !_IsHas(line, ".png")){
-
-	  cout<<line<<endl;
-
-	}
-	else{
-	  if(_IsHas(line, ".png")){
-	    texture_file_name = line;
-	  }
-	  else{
-
-	    _CreateValuePair(line);
-
-	  }
-	}
-
+	line_list.push_back(line);
       }
       file.close();
     }
     else{
-      cout<<"unable to open file!"<<endl;
+      cout<<"Unable to open "<<file_src<<" file!"<<endl;
     }
+
+    _ReadSequence();
+
+    line_list.clear();
   }
 
   Rectangle FindRegion(string name);
   Texture2D CreateTexture(string name);
 
 protected:
+
+  enum Status{
+    GET_STARTED,
+    GET_SECTION,
+    GET_EOF
+  };
+
+  Status status = GET_STARTED;
+  
+  vector<string> line_list;
   map<string, string*> value_pair_map; // texture atlas attribute value pair
   map<string, AtlasRegion> region_map;
 
-  inline void _CreateValuePair(string line){
+  inline void _CreateValuePair(string line, string *attribute, string *value){
     size_t colon_index = line.find(":");
-    string attribute = line.substr(0, colon_index);
-    string value = line.substr(colon_index + 1, line.length());
+    *attribute = line.substr(0, colon_index);
+    *value = line.substr(colon_index + 1, line.length());
+  }
 
-    cout<<attribute<<", "<<value<<endl;
+  inline string* _GetNumOfValue(string value){
+    string* num_of_value = nullptr; 
+    size_t comma_index = value.find(",");
+    if(comma_index != string::npos){
+      *num_of_value = "null";
+    }
+    else{
+      num_of_value = &value;
+    }
+    return num_of_value; 
   }
 
   inline bool _IsHas(string line, string sub_str){
@@ -103,6 +111,38 @@ protected:
       return true;
     }
     return false;
+  }
+
+  inline bool _IsSection(string line){
+    if(!_IsHas(line, ":") &&
+       !_IsHas(line, ",") &&
+       !_IsHas(line, ".png")){
+      return true;
+    }
+    return false;
+  }
+
+  inline void _ReadSequence(){
+	
+    switch(status){
+    case GET_STARTED:
+      for(int i = 0; i < line_list.size(); i++){
+	if(_IsHas(line_list.at(i), ".png")){
+	  texture_file_name = line_list.at(i);
+	  cout<<"get >> texture file name:"<<texture_file_name<<endl;
+	}
+	else{
+	  string attribute;
+	  string value;
+	  _CreateValuePair(line_list.at(i), &attribute, &value);
+	  cout<<"get >> "<<attribute<<" >> "<<value<<endl;
+	}
+      }
+      break;
+
+    case GET_SECTION:
+      break;
+    }
   }
 
   inline void _Insert(string name, AtlasRegion atlas_region){
